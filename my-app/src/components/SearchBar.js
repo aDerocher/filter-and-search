5e604e960738412aa290ed8bf4d4adc4f8713868
aDerocher <am.derocher@gmail.com>
2022-02-11 09:29:48 -0800

    working

--- my-app/src/components/SearchBar.js
+++ my-app/src/components/SearchBar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import data from './../data/clothes.json'
+import './../styles/style.css'
 
 const SearchBar = () => {
 
@@ -7,6 +8,7 @@ const SearchBar = () => {
 	const [ list, setList ] = useState([])
 	const [ searchVal, setSearchVal ] = useState("")
 	const [ colorFilter, setColorFilter ] = useState([])
+	const [ sizeFilter, setSizeFilter ] = useState([])
 
 	useEffect(() => {
 		setList(clothesData)
@@ -16,17 +18,21 @@ const SearchBar = () => {
 		if(searchVal === ""){ setList(clothesData) }
 		if(!clothesData) { return }
 		let newList = [...clothesData].filter((item)=> {
+
 			if(colorFilter.length > 0 && !colorFilter.includes(item.color)){
 				return false
 			}
-			if(item.type.includes(searchVal) || 
+			if(sizeFilter.length > 0 && !sizeFilter.includes(item.size)){
+				return false
+			}
+			if(item.type.includes(searchVal) ||
 				item.color.includes(searchVal) ||
 				item.size.includes(searchVal)){
 					return true
 			}
 		})
 		setList(newList)
-	},[searchVal, colorFilter])
+	},[searchVal, colorFilter, sizeFilter])
 
 	const handleColorFilter = (color) => {
 		let colors = [...colorFilter]
@@ -40,21 +46,70 @@ const SearchBar = () => {
 		setColorFilter(colors)
 		return
 	}
+	const handleSizeFilter = (size) => {
+		let sizes = [...sizeFilter]
+		if (sizes.indexOf(size) < 0){
+			sizes.push(size)
+		} else {
+			sizes = sizes.filter((col) => {
+				return col !== size
+			})
+		}
+		setSizeFilter(sizes)
+		return
+	}
 
 	return(
 		<>
-			<input type='text' value={searchVal} onChange={e=>setSearchVal(e.target.value)}></input>
-			<br />
-			<label>Red</label>
-			<input type='checkbox' value='red' onChange={e=>handleColorFilter('red')}></input>
-			<br />
-			<label>Orange</label>
-			<input type='checkbox' value='orange' onChange={e=>handleColorFilter('orange')}></input>
-			<ul>
-				{list && list.map((item, i)=>(
-					<li key={i}><span style={{color: `${item.color}`}}>{item.color}</span> {item.type} ({item.size})</li>
-				))}
-			</ul>
+		<div className='container'>
+			<div className='filters'>
+				<section>
+					<label>Search </label>
+					<input type='text' value={searchVal} onChange={e=>setSearchVal(e.target.value)}></input>
+				</section>
+
+				<section>
+					<label>Red</label>
+					<input type='checkbox' value='red' onChange={e=>handleColorFilter(e.target.value)}></input>
+					<br />
+					<label>Orange</label>
+					<input type='checkbox' value='orange' onChange={e=>handleColorFilter(e.target.value)}></input>
+					<br />
+					<label>Green</label>
+					<input type='checkbox' value='green' onChange={e=>handleColorFilter(e.target.value)}></input>
+					<br />
+					<label>Blue</label>
+					<input type='checkbox' value='orange' onChange={e=>handleColorFilter(e.target.value)}></input>
+					<br />
+					<label>Grey</label>
+					<input type='checkbox' value='grey' onChange={e=>handleColorFilter(e.target.value)}></input>
+				</section>
+				<section>
+					<label>Small</label>
+					<input type='checkbox' value='sm' onChange={e=>handleSizeFilter('sm')}></input>
+					<br />
+					<label>Medium</label>
+					<input type='checkbox' value='md' onChange={e=>handleSizeFilter('md')}></input>
+					<br />
+					<label>Large</label>
+					<input type='checkbox' value='lg' onChange={e=>handleSizeFilter('lg')}></input>
+					<br />
+					<label>X-Large</label>
+					<input type='checkbox' value='xl' onChange={e=>handleSizeFilter('xl')}></input>
+				</section>
+			</div>
+
+			<div className='data'>
+				<ul>
+					{!list &&
+						<p>No Results Found</p>
+					}
+					{list && list.map((item, i)=>(
+						<li key={i}><span style={{color: `${item.color}`}}>{item.color}</span> {item.type} ({item.size})</li>
+						))}
+				</ul>
+			</div>
+		</div>
 		</>
 	)
 };
